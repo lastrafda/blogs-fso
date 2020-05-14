ff8a773440adad419a261b7f33ee956a1fa90787
José Lastra <lastrafda@gmail.com>
2020-05-14 09:30:03 -0400

    Added tests for list_helper@totalLikes

---
+++ utils/list_helper.js
@@ -0,0 +1,11 @@
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+  totalLikes
+}
