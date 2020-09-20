d23d13fd46748de197d702ed6c9d4ae112ed5135
José Lastra <lastrafda@gmail.com>
2020-09-20 17:36:43 -0300

    include user info when fetching blogs

--- controller/blogs.js
+++ controller/blogs.js
@@ -2,15 +2,15 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs.map((blog) => blog.toJSON()))
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
   try {
     const savedBlog = await blog.save()
-    response.status(201).json(savedBlog.toJSON())
+    response.status(201).json(savedBlog)
   } catch (error) {
     next(error)
   }
@@ -25,7 +25,7 @@ blogsRouter.get('/:id', async (request, response, next) => {
   try {
     const blog = await Blog.findById(request.params.id)
     if (blog) {
-      response.json(blog.toJSON())
+      response.json(blog)
     } else {
       response.status(404).end()
     }
@@ -47,14 +47,14 @@ blogsRouter.put('/:id', async (request, response, next) => {
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
       new: true,
     })
-    response.json(updatedBlog.toJSON())
+    response.json(updatedBlog)
   } catch (error) {
     next(error)
   }
   /** This is how it looks without async/await */
   // Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   //   .then((updatedBlog) => {
-  //     response.json(updatedBlog.toJSON())
+  //     response.json(updatedBlog)
   //   })
   //   .catch((error) => next(error))
 })
